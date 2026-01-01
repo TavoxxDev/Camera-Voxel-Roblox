e424457383313f1bf745d28188931327b0f1905c
SrBolasGrandes <canalpalomacabesuda@gmail.com>
2026-01-01 12:23:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,13 +13,13 @@ last_frame = None
 last_time = 0
 last_smile = False
 
-FALLBACK_IMAGE_URL = "https://raw.githubusercontent.com/SrBolasGrandes/Camera-Voxel-Roblox/refs/heads/main/262%20Sem%20T%C3%ADtulo_20260101105003.png"
+FALLBACK = "https://raw.githubusercontent.com/SrBolasGrandes/Camera-Voxel-Roblox/refs/heads/main/262%20Sem%20T%C3%ADtulo_20260101105003.png"
 
-def load_fallback():
+def fallback():
     global last_frame
-    r = requests.get(FALLBACK_IMAGE_URL, timeout=5)
+    r = requests.get(FALLBACK)
     img = Image.open(io.BytesIO(r.content)).convert("RGB")
-    img = img.resize((GRID, GRID), Image.BILINEAR)
+    img = img.resize((GRID, GRID))
     last_frame = list(img.getdata())
 
 @app.route("/")
@@ -30,36 +30,30 @@ def home():
 def camera():
     global last_frame, last_time, last_smile
 
-    mode = request.json.get("mode", "color")
-    smiling = request.json.get("smiling", False)
     raw = base64.b64decode(request.json["image"])
+    mode = request.json.get("mode", "color")
+    last_smile = request.json.get("smiling", False)
 
     img = Image.open(io.BytesIO(raw)).convert("RGB")
-    img = img.resize((GRID, GRID), Image.BILINEAR)
+    img = img.resize((GRID, GRID))
 
     pixels = list(img.getdata())
 
     if mode == "bw":
         pixels = [
             (l := int(r*0.299 + g*0.587 + b*0.114), l, l)
-            for r, g, b in pixels
+            for r,g,b in pixels
         ]
 
     last_frame = pixels
-    last_smile = smiling
     last_time = time.time()
 
     return jsonify(ok=True)
 
 @app.route("/cameraGet")
 def camera_get():
-    global last_frame
-
     if last_frame is None or time.time() - last_time > TIMEOUT:
-        load_fallback()
-
-    if last_frame is None:
-        return jsonify(ready=False)
+        fallback()
 
     return jsonify(
         ready=True,
